f9854163372b1ff65ee7579622200251a7011d7a
Shinya Yajima <y.megane88@gmail.com>
2024-07-12 18:04:02 +0900

    wip

--- remix-tutorial/app/root.tsx
+++ remix-tutorial/app/root.tsx
@@ -1,12 +1,25 @@
+import { json } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
+import appStylesHref from "./app.css?url";
 import {
   Form,
   Links,
   Meta,
   Scripts,
+  Outlet,
   ScrollRestoration,
+  Link,
+  useLoaderData,
 } from "@remix-run/react";
+import { getContacts } from "./data";
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -33,16 +46,31 @@ export default function App() {
               <button type="submit">New</button>
             </Form>
           </div>
-          <nav>
+          {contacts.length ? (
             <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite ? <span>★</span> : null}
+                  </Link>
+                </li>
+              ))}
             </ul>
-          </nav>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </div>
+        <div id="detail">
+          <Outlet />
         </div>
 
         <ScrollRestoration />
@@ -51,3 +79,7 @@ export default function App() {
     </html>
   );
 }
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
